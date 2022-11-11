c382c230227f871a419861f8efb5dabb0f9e74e2
User <User@erg1-pc11>
2022-11-11 15:48:32 +0200

    first..

---
+++ src/main/java/com/iek/mathima1/MATHIMA1.java
@@ -0,0 +1,21 @@
+
+
+package com.iek.mathima1;
+
+
+public class MATHIMA1 {
+
+    public static void main(String[] args) {
+        
+        int a = 4;
+        int b = 3;
+        if(a > b){
+            System.out.println("a is greater than b");
+        } else{
+            System.out.println("looser");
+        }
+        
+        
+        System.out.println("Hello World!");
+    }
+}
