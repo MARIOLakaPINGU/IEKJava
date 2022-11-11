0412a2a3286cf73d530b9009050c31181fdd3a19
User <User@erg1-pc11>
2022-11-11 17:43:03 +0200

    first..

--- src/main/java/com/iek/mathima1/MATHIMA1.java
+++ src/main/java/com/iek/mathima1/MATHIMA1.java
@@ -3,19 +3,49 @@
 package com.iek.mathima1;
 
 
-public class MATHIMA1 {
+import java.util.Scanner;
 
+public class MATHIMA1 {
+    
+    
+    
     public static void main(String[] args) {
         
-        int a = 4;
-        int b = 3;
-        if(a > b){
-            System.out.println("a is greater than b");
-        } else{
-            System.out.println("looser");
-        }
+        String name;
+        String lastName;
+        int age;
+        char gender;
+        double salary;
         
         
-        System.out.println("Hello World!");
+        Scanner console = new Scanner(System.in);
+        
+        System.out.println("Enter name: ");
+        
+        name = console.nextLine();
+        
+        System.out.println("Enter lastname: ");
+        
+        lastName = console.nextLine();
+       
+       
+        System.out.println("Enter age: ");
+        age = console.nextInt();
+        
+        
+        System.out.println("Enter gender: (M/F) ");
+        gender = console.next().charAt(0);
+        
+        System.out.println("Enter salary: ");
+        salary = console.nextDouble();
+        
+        System.out.println("Name: " + name +" Lastname: " + lastName + " Age: " + age + " Gender: "+ gender + " Salary: " + salary);
+        
+        
+        
+        
+        
+        
     }
 }
+
